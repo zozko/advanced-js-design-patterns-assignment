d5a036620b03cc2ff39bba310a1f0e77b0c5d7a7
Anikó Fejes <oranjje@gmail.com>
2020-10-13 15:30:57 +0200

    refactor(shop): use the constructor pattern

--- module.js
+++ module.js
@@ -1,81 +1,82 @@
-// UI module for changing the user interface
-const UI = (() => {
+// UI constructor for changing the user interface
+const UI = function () {
 
   // this is basically our configuration,
   // the part that might have to change when the HTML changes:
   // gathering the selectors for the elements we will be working with
-  const productImageSelector = ".js-product-image";
-  const productNameSelector = ".js-product-name";
-  const productCategorySelector = ".js-product-category";
-  const productDescriptionSelector = ".js-product-description";
-  const productPriceSelector = ".js-product-price";
+  this.productImageSelector = ".js-product-image";
+  this.productNameSelector = ".js-product-name";
+  this.productCategorySelector = ".js-product-category";
+  this.productDescriptionSelector = ".js-product-description";
+  this.productPriceSelector = ".js-product-price";
 
   // using the selectors above, we store the elements in private variables
-  const productImageElement = document.querySelector(productImageSelector);
-  const productNameElement = document.querySelector(productNameSelector);
-  const productCategoryElement = document.querySelector(productCategorySelector);
-  const productDescriptionElement = document.querySelector(productDescriptionSelector);
-  const productPriceElement = document.querySelector(productPriceSelector);
+  this.productImageElement = document.querySelector(this.productImageSelector);
+  this.productNameElement = document.querySelector(this.productNameSelector);
+  this.productCategoryElement = document.querySelector(this.productCategorySelector);
+  this.productDescriptionElement = document.querySelector(this.productDescriptionSelector);
+  this.productPriceElement = document.querySelector(this.productPriceSelector);
 
-  // this is the public API for our UI module:
-  // methods to change the UI by only changing the
-  // content of the privately stored elements
-  return {
-    setProductImage: (src) => {
-      productImageElement.src = src;
-    },
-    setProductName: (name) => {
-      productNameElement.textContent = name;
-    },
-    setProductCategory: (category) => {
-      productCategoryElement.textContent = category;
-    },
-    setProductDescription: (description) => {
-      productDescriptionElement.textContent = description;
-    },
-    setProductPrice: (price) => {
-      productPriceElement.textContent = price;
-    },
-  };
-})();
+};
 
-// Product module for gathering product related info
-const Product = (() => {
-  const name = "DisCatcher Target";
-  const category = " Discgolf";
-  const description =
+// this is the API for our UI objects:
+// methods to change the UI by only changing the
+// content of the stored elements
+UI.prototype.setProductImage = function (src) {
+  this.productImageElement.src = src;
+};
+UI.prototype.setProductName = function (name) {
+  this.productNameElement.textContent = name;
+};
+UI.prototype.setProductCategory = function (category) {
+  this.productCategoryElement.textContent = category;
+};
+UI.prototype.setProductDescription = function (description) {
+  this.productDescriptionElement.textContent = description;
+};
+UI.prototype.setProductPrice = function (price) {
+  this.productPriceElement.textContent = price;
+};
+
+// Product constructor for gathering product related info
+const Product = function () {
+  this.name = "DisCatcher Target";
+  this.category = " Discgolf";
+  this.description =
     "a chain grid that catches fast and slow putts, heavy and light discs like no other target";
-  const imageSrc = "images/discatcher.jpg";
-  const price = 399;
+  this.imageSrc = "images/discatcher.jpg";
+  this.price = 399;
+};
 
-  // public API for Product module for getting product info
-  return {
-    getName: () => {
-      return name;
-    },
-    getCategory: () => {
-      return category;
-    },
-    getDescription: () => {
-      return `${name} is ${description}.`;
-    },
-    getImage: () => {
-      return imageSrc;
-    },
-    getPrice: () => {
-      return `€${price}`;
-    }
-  }
-})();
+// API for Product objects for getting product info
+Product.prototype.getName = function () {
+  return this.name;
+};
+Product.prototype.getCategory = function () {
+  return this.category;
+};
+Product.prototype.getDescription = function () {
+  return `${this.name} is ${this.description}.`;
+};
+Product.prototype.getImage = function () {
+  return this.imageSrc;
+};
+Product.prototype.getPrice = function () {
+  return `€${this.price}`;
+};
 
-const productImage = Product.getImage();
-const productName = Product.getName();
-const productCategory = Product.getCategory();
-const productDescription = Product.getDescription();
-const productPrice = Product.getPrice();
+const DiscgolfProduct = new Product();
 
-UI.setProductImage(productImage);
-UI.setProductName(productName);
-UI.setProductCategory(productCategory);
-UI.setProductDescription(productDescription);
-UI.setProductPrice(productPrice);
+const productImage = DiscgolfProduct.getImage();
+const productName = DiscgolfProduct.getName();
+const productCategory = DiscgolfProduct.getCategory();
+const productDescription = DiscgolfProduct.getDescription();
+const productPrice = DiscgolfProduct.getPrice();
+
+const ShopUI = new UI();
+
+ShopUI.setProductImage(productImage);
+ShopUI.setProductName(productName);
+ShopUI.setProductCategory(productCategory);
+ShopUI.setProductDescription(productDescription);
+ShopUI.setProductPrice(productPrice);
